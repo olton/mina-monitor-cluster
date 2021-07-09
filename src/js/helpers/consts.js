d82cdf783ea889a305e3aa374b1c0ae057653ef4
Sergey Pimenov <sergey@pimenov.com.ua>
2021-07-09 11:26:20 +0300

    improves

--- src/js/helpers/consts.js
+++ src/js/helpers/consts.js
@@ -1,4 +1,4 @@
 export const version = `1.0.0`
-export const title = `Mina Monitor %VER% :: Multinode`
+export const title = `Mina Monitor Cluster %VER%`
 export const imgStop = `<span class="mif-stop label-alert">`
 export const imgOk = `<span class="mif-checkmark label-success">`

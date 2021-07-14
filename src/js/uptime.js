3544d55a44509c9d7fba484bbfbb9c0eba05f82a
Sergey Pimenov <sergey@pimenov.com.ua>
2021-07-14 08:15:51 +0300

    fixed trimming address for uptime

--- src/js/uptime.js
+++ src/js/uptime.js
@@ -31,7 +31,9 @@ export const getUptime = async () => {
         $("#position-icon").removeClassBy("label-").removeClassBy("mif-").addClass(`label-${color}`).addClass(`mif-${icon}`)
         $("#uptime-rate").text((parseFloat(rate)) + "%")
         $("#uptime-score").text(score)
-        $("#uptime-key").html( shortAddress(publicKey) )
+        $("#uptime-key").html( shortAddress(publicKey.trim()) )
+
+        console.log(publicKey.trim())
 
         elLog.html(imgOk)
     } else {

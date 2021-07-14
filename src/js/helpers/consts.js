893ec5300cf3d476c758c5812273d11a44ebb16a
Sergey Pimenov <sergey@pimenov.com.ua>
2021-07-14 08:33:24 +0300

    version changed to 1.0.2

--- src/js/helpers/consts.js
+++ src/js/helpers/consts.js
@@ -1,4 +1,4 @@
-export const version = `1.0.1`
+export const version = `1.0.2`
 export const title = `Mina Monitor Cluster %VER%`
 export const imgStop = `<span class="mif-stop label-alert">`
 export const imgOk = `<span class="mif-checkmark label-success">`

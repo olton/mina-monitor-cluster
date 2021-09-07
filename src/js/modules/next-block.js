93e0a53237556444ec58f1ddfd98e30ca53b0bc5
Sergey Pimenov <sergey@pimenov.com.ua>
2021-09-07 11:54:15 +0300

    fix module next-block for zero value

--- src/js/modules/next-block.js
+++ src/js/modules/next-block.js
@@ -1,6 +1,5 @@
 export const processNextBlock = (i, node, data) => {
     if (isNaN(data)) return
-    if (!data) return
 
     if (isNaN(state.nextBlock) || state.nextBlock !== data) {
         state.nextBlock = data

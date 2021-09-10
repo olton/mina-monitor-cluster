b6f458cee927d61cd24c645d5bb6429c1d77b8a1
Sergey Pimenov <sergey@pimenov.com.ua>
2021-09-10 10:08:58 +0300

    fixed balance updates

--- src/js/modules/balance.js
+++ src/js/modules/balance.js
@@ -7,7 +7,7 @@ export const processBalance = (i, node, data) => {
 
     const {balance, timing} = data.data.account
 
-    if (!state.balance.total || (balance.total > state.balance.total) ) {
+    if (!state.balance.total || (balance.total !== state.balance.total) ) {
         state.balance = balance
         state.timing = timing
     }

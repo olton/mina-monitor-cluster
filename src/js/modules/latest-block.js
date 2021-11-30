5b5ab6bc710bd50a0e72c7080aab42ec4f115391
Sergey Pimenov <sergey@pimenov.com.ua>
2021-11-30 14:40:27 +0200

    Added data from Mina Explorer: latest block, current block height

--- src/js/modules/latest-block.js
+++ src/js/modules/latest-block.js
@@ -1,7 +1,7 @@
 import {shortAddress} from "../helpers/utils";
+import {MINA_EXPLORER, STAKETAB_EXPLORER} from "../helpers/consts";
 
 export const processLatestBlocks = (i, node, data) => {
-    console.log(!data, !Array.isArray(data), !data.length)
     if (!data || !Array.isArray(data) || !data.length) return
 
     state.latestBlocks = data[0]
@@ -10,8 +10,6 @@ export const processLatestBlocks = (i, node, data) => {
 export const updateLatestBlocks = () => {
     if (!state.latestBlocks) return
 
-    console.log(state.latestBlocks)
-
     const {
         blockHeight = 0,
         creator = "",
@@ -19,12 +17,25 @@ export const updateLatestBlocks = () => {
     } = state.latestBlocks
 
     const elLatestBlockHeight = $("#latest-block-height")
-    const elLatestBlockHeightParent = elLatestBlockHeight.parent()
+    const elCreatorBlockMedal = $("#creator-block-medal")
     const elLatestBlockCreator = $("#latest-block-creator")
     const elLatestBlockTime = $("#latest-block-datetime")
     const blockDate = datetime(dateTime)
+    const {explorer = "staketab"} = config
 
     elLatestBlockHeight.html((+blockHeight).format(0, null, " ", "."))
-    elLatestBlockCreator.html(shortAddress(creator))
+    elLatestBlockCreator.html(
+        $("<a>")
+            .addClass("no-decor fg-accent")
+            .attr("href", (explorer.toLowerCase() === "mina" ? MINA_EXPLORER : STAKETAB_EXPLORER) + creator )
+            .html(shortAddress(creator))
+    )
     elLatestBlockTime.html(blockDate.format("HH:mm"))
+
+    const {address = ""} = state.uptime
+
+    elCreatorBlockMedal.hide()
+    if (creator === address) {
+        elCreatorBlockMedal.show()
+    }
 }

1ca3c7cd28dd83ffc524a0dcefae2afb17171bf0
Sergey Pimenov <sergey@pimenov.com.ua>
2021-12-22 19:28:12 +0200

    change version to 2.1.0

--- src/js/index.js
+++ src/js/index.js
@@ -29,7 +29,7 @@ import {processExplorerSummary} from "./modules/explorer-summary";
 import {processExplorerHeight} from "./modules/explorer-height";
 import {processHttps} from "./modules/https";
 
-const version = `2.0.0`
+const version = `2.1.0`
 
 $("title").text(TITLE.replace('%VER%', version))
 $("#version").text(version)
